3ce6de04c8f52e2183e341823c4c52c987a85698
Devband777 <world.devband777@gmail.com>
2023-05-12 22:03:59 +0200

    Email sign up complete

--- src/components/EmailSignUp/index.jsx
+++ src/components/EmailSignUp/index.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'route-lite';
 import logo from '../../assets/img/skipimage.png';
+import Landing from '../Landing';
 import './style.scss';
 export default function EmailSignUp() {
+  const [isSecondClick, setIsSecondClick] = useState(false);
+  const handleClick = () => {
+    setIsSecondClick(true);
+  };
   return (
     <div className="emailsignup">
-      <div className="emailsignup-input">
+      <div className={`emailsignup-input ${isSecondClick ? 'hidden' : ''}`}>
         <div className="emailsignup-input-label">
           <span>*</span>Email
         </div>
@@ -12,8 +18,31 @@ export default function EmailSignUp() {
           <input placeholder="ex: hello@gmail.com" type="email" />
         </div>
       </div>
-      <div className="emailsignup-button">
+      <div className={`emailsignup-input ${isSecondClick ? '' : 'hidden'}`}>
+        <div className="emailsignup-input-label">
+          <span>*</span>Username
+        </div>
+        <div className="emailsignup-input-input">
+          <input placeholder="ex: something unique" type="text" />
+        </div>
+      </div>
+      <div className={`emailsignup-input ${isSecondClick ? '' : 'hidden'}`}>
+        <div className="emailsignup-input-label">
+          <span>*</span>Password
+        </div>
+        <div className="emailsignup-input-input">
+          <input placeholder="input password" type="password" />
+        </div>
+      </div>
+      <div
+        className={`emailsignup-button ${isSecondClick ? 'hidden' : ''}`}
+        onClick={handleClick}
+      >
         Continue
+        <img src={logo} alt="logo" />
+      </div>
+      <div className={`emailsignup-button ${isSecondClick ? '' : 'hidden'}`}>
+        <Link component={Landing}>Continue</Link>
         <img src={logo} alt="logo" />
       </div>
     </div>
